86116e60b5dd9c9dea948b560fc896b347f82de6
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2023-06-18 17:20:24 +0600

    Video Player

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Alpha Player'),
-          backgroundColor: Colors.black26,
+          backgroundColor: Colors.teal[900],
         ),
         body: MyPlayerPage(),
       ),
@@ -23,11 +25,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyPlayerPage extends StatelessWidget {
+class MyPlayerPage extends StatefulWidget {
   const MyPlayerPage({Key? key}) : super(key: key);
 
   @override
+  State<MyPlayerPage> createState() => _MyPlayerPageState();
+}
+
+class _MyPlayerPageState extends State<MyPlayerPage> {
+  late VideoPlayerController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4")
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: _controller.value.isInitialized
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      padding: EdgeInsets.all(16),
+                    ),
+                      onPressed: () {
+                        setState(() {
+                          _controller.value.isPlaying
+                              ? _controller.pause()
+                              : _controller.play();
+                        });
+                      },
+                      child: Icon(
+                        _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                      ),),
+                ],
+              )
+            : Container());
   }
 }
---
+++ lib/words.dart
@@ -0,0 +1,5 @@
+import 'package:english_words/english_words.dart';
+
+void main(){
+  nouns.take(50).forEach(print);
+}
